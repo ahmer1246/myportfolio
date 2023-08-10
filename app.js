48ec3fc1290ded343c4e82cdbdd09a0ad4053889
ahmer1246 <ahmer1246@gmail.com>
2023-08-11 00:28:04 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+navListEl = document.querySelector('.navlist');
+navConEl = document.querySelector('.navCon');
+humburger = document.querySelector('.humburger');
+
+
+humburger.addEventListener('click',()=>{
+    navListEl.classList.toggle('opt-nav');
+    // navConEl.classList.toggle('h-nav')
+}
+)
+
